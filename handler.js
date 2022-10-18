bcaceffd80127b1203bcf14b88b121d9c1f4a666
Diluc <91557468+Dilucal@users.noreply.github.com>
2022-10-18 08:28:22 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -213,7 +213,7 @@ module.exports = async (client, m) => {
             if (!m.isGroup && chats && !isPrem && !users.banned && setting.groupmode) return client.sendMessageModify(m.chat, `🚩 Using bot in private chat only for premium user, upgrade to premium plan only Rp. 5,000,- to get 1K limits.\n\nIf you want to buy contact *${prefixes[0]}owner*`, m, {
                largeThumb: true,
                thumbnail: await Func.fetchBuffer('https://telegra.ph/file/0b32e0a0bb3b81fef9838.jpg'),
-               url: 'https://chat.whatsapp.com/Dh1USlrqIfmJT6Ji0Pm2pP'
+               url: 'https://chat.whatsapp.com/CmJkktfVp0H2Gl4v11VOLW'
             }).then(() => chats.lastchat = new Date() * 1)
             if (setting.self && !['chatAI', 'exec'].includes(name) && !isOwner && !m.fromMe) continue
             if (!m.isGroup && ['chatAI'].includes(name) && body && Func.socmed(body)) continue
